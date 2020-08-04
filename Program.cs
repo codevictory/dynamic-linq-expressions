6379d33235bce149bc3c888a818605aceb9f8bda
codevictory <main@codevictory.net>
2020-08-04 19:23:47 +0300

    Initial dotnet console app.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dynamic_linq_expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
